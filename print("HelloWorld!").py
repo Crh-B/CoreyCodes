41df5910bb81537b812bec5474ecb965bd3d5c9e
Alex McIntosh <alex.mcintosh@krakentechnologies.ltd>
2023-02-20 22:22:42 +0000

    Added some excersises for Corey to work on

--- print("HelloWorld!").py
+++ print("HelloWorld!").py
@@ -1,1 +1,9 @@
 print("HelloWorld!")
+
+# Excesises for Corey.
+# 1) Make a short story about yourself with the information stored as each of the different variable 
+# types we talked about. Make sure to use at least 5 different variable types.
+# 2) Make a list at least 10 elements long. Print out the list, then print out the first 3 elements of the list.
+# then print the list in reverse order. Then print out the last 3 elements of the list.
+# 3) Make a variable integer then use it in an if statement to find if it is odd or even.
+# EXTRA : Do the same thing as in ex 1 but use a dictionary instead of variables.
